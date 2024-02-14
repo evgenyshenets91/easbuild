2fc1717a652f8b77339ef56db400c4ae048d8cae
Evgeny Shenets <evgenyshenets91@gmail.com>
2024-02-14 19:42:15 +0300

    add version app and hide action

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Application from 'expo-application';
+
 
 export default function App() {
+  const getAppVersion =  () => {
+    return Application.applicationName === 'Expo Go' ? require('./app.json').expo.version : Application.nativeApplicationVersion
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>App version {getAppVersion()}</Text>
     </View>
   );
 }
